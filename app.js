0637fa168f88125511bdfcba960854ce4affb96d
EnricoClem <enrico.clem94@gmail.com>
2024-03-04 16:51:09 +0100

    ADD - if else calculation of age discount

--- app.js
+++ app.js
@@ -13,7 +13,17 @@ let km_value = (km * 0.21); //number
 
 // Modificare il risultato in base all'età dell'utente
 // SE l'età è sotto i 18 togliere il 20% dal risultato
+if(age <= 17){
+    let under_18 = (km_value * 80 / 100); //number
+    console.log(under_18)
 // ALTRIMENTI SE è superiore a 18 ma inferiore a 65 non modificare la cifra
-// ALTRIMENTI oltre il 65 togliere il 40% dal risultato
+} else if(age > 17 && age < 65){
+    let standard = (km_value) //number
+    console.log(standard)
+    // ALTRIMENTI oltre il 65 togliere il 40% dal risultato
+} else{
+    let over_65 = (km_value * 60 /100) //numbers
+    console.log(over_65)
+}
 
 // Pulire il risultato assicurando che i centesimi non siano mostrati con non oltre due decimali e applicare il valore €
