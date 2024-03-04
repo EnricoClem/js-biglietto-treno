98b1624628fad8d7027b6d5719b0dc898b13f844
EnricoClem <enrico.clem94@gmail.com>
2024-03-04 15:29:38 +0100

    SETTING - organizing base workspace and assignment

---
+++ app.js
@@ -0,0 +1,3 @@
+// Creare un programma che in base ai kilometri da percorrere e all'età calcoli il prezzo del bigietto.
+// Il prezzo del biglietto è di 0.21 € per km.
+// Va applicato uno sconto del 20% per i minorenni ed uno sconto del 40% per gli over 65.
