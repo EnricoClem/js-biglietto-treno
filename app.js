d631858ea8810083585f8cc39d928822cd26cf76
EnricoClem <enrico.clem94@gmail.com>
2024-03-04 17:23:58 +0100

    ADD - math floor to remove decimals

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Creare un programma che in base ai kilometri da percorrere e all'età calcoli il prezzo del bigietto.
 // Il prezzo del biglietto è di 0.21 € per km.
 // Va applicato uno sconto del 20% per i minorenni ed uno sconto del 40% per gli over 65.
-console.log('Ciao mondo');
+console.log('Ciao, questo è il costo del tuo biglietto:');
 
 // OTTEENRE il costo in base ai kilometri
 // Chiedere all'utente quanti kilometri vuole percorrere e qual'è la sua età
@@ -15,15 +15,18 @@ let km_value = (km * 0.21); //number
 // SE l'età è sotto i 18 togliere il 20% dal risultato
 if(age <= 17){
     let under_18 = (km_value * 80 / 100); //number
-    console.log(under_18)
+    let ticket = Math.floor (under_18 * 100) / 100; //number
+    console.log(ticket); //number
+
 // ALTRIMENTI SE è superiore a 18 ma inferiore a 65 non modificare la cifra
 } else if(age > 17 && age < 65){
-    let standard = (km_value) //number
-    console.log(standard)
-    // ALTRIMENTI oltre il 65 togliere il 40% dal risultato
+    let standard = (km_value); //number
+    let ticket = Math.floor (standard * 100) / 100; //number
+    console.log(ticket); //number
+    
+// ALTRIMENTI oltre il 65 togliere il 40% dal risultato
 } else{
-    let over_65 = (km_value * 60 /100) //numbers
-    console.log(over_65)
+    let over_65 = (km_value * 60 /100); //numbers
+    let ticket = Math.floor (over_65 * 100) / 100; //number
+    console.log(ticket + ' €'); //number
 }
-
-// Pulire il risultato assicurando che i centesimi non siano mostrati con non oltre due decimali e applicare il valore €
